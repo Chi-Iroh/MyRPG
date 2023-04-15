3996eee863eb4f30f45114bb9c40e3080d37e0bd
Thomas Sayen <thomas.sayen@epitech.eu>
2023-04-15 10:10:15 +0200

    Fixed coding style in button lib ; Fixed button title resizing.

--- lib/button/button_draw.c
+++ lib/button/button_draw.c
@@ -13,9 +13,7 @@ static void draw_button_title(sfRenderWindow *window, button_t *button)
     const unsigned char_size = sfText_getCharacterSize(button->text);
     const sfVector2f above_pos = {
         .x = button->area.left,
-
-        // casted to int to prevent underflow when button->area.top is 0
-        .y = (int)(button->area.top - char_size)
+        .y = button->area.top - char_size
     };
     const sfVector2f beneath_pos = {
         .x = button->area.left,
@@ -29,13 +27,7 @@ static void draw_button_title(sfRenderWindow *window, button_t *button)
     } else {
         sfText_setPosition(button->text, *pos);
     }
-    sfText_setCharacterSize(button->text, button->text_size);
-    sfText_setFont(button->text, button->font);
-    if (button->use_utf8_title) {
-        sfText_setUnicodeString(button->text, button->title.utf8);
-    } else {
-        sfText_setString(button->text, button->title.ansi);
-    }
+    update_button_title(button);
     sfText_setFillColor(button->text, button->text_color);
     sfRenderWindow_drawText(window, button->text, NULL);
 }
@@ -53,14 +45,12 @@ void draw_button(sfRenderWindow *window, button_t *button)
         }
         sfRectangleShape_setFillColor(button->shape, sfWhite);
         sfRectangleShape_setTexture(button->shape, button->fill.texture, true);
-    } else {
+    } else if (button->fill_mode == BUTTON_FILL_WITH_COLOR) {
         sfRectangleShape_setFillColor(button->shape, button->fill.color);
     }
     convert_rectangle_shape_to_float_rect(button->shape, &button->area);
     sfRenderWindow_drawRectangleShape(window, button->shape, NULL);
-    if (button->fill_mode == BUTTON_FILL_UNINITIALIZED) {
-        return;
-    } else if (button->title_pos != BUTTON_TITLE_HIDDEN) {
+    if (button->title_pos != BUTTON_TITLE_HIDDEN) {
         draw_button_title(window, button);
     }
 }
