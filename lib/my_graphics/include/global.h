3086b5ab85ee18b94b509ccdcf5fb19bcabe5a40
PERRIOLAT Nils <nils.perriolat@epitech.eu>
2023-04-17 09:02:29 -0400

    [FIX] : global.h missing

---
+++ lib/my_graphics/include/global.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** library MY_GRAPHICS
+** File description:
+** header for global variable
+*/
+#ifndef MY_GRAPHICS_GLOBAL
+    #define MY_GRAPHICS_GLOBAL
+    /*
+    ** ============================== GLOBAL ==============================
+    */
+    static int current_id = 0;
+#endif
